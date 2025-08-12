beea4d1c8cca5b2eecc34b0d81600938389997a2
Rohan Singla <rohansinglawork@gmail.com>
2025-08-12 18:01:56 +0530

    contract

--- sol-counter/src/lib.rs
+++ sol-counter/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+entrypoint!(counter_contract);
+
+#[derive(BorshDeserialize, BorshSerialize)]
+struct Counter {
+    count: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(BorshSerialize, BorshDeserialize)]
+enum CounterInstruction {
+    Increment(u32),
+    Decrement(u32),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn counter_contract(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let account = next_account_info(&mut accounts.iter())?;
+    let mut counter = Counter::try_from_slice(&account.data.borrow())?;
+
+    match CounterInstruction::try_from_slice(instruction_data)? {
+        CounterInstruction::Increment(amount) => {
+            counter.count += amount;
+        }
+        CounterInstruction::Decrement(amount) => {
+            counter.count -= amount;
+        }
     }
+
+    counter.serialize(&mut *account.data.borrow_mut())?;
+    msg!("Counter updated to {}", counter.count);
+    Ok(())
 }
