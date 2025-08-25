c76ce4de55db6d0e289fcc2c3897b37ce3948075
Rohan Singla <rohansinglawork@gmail.com>
2025-08-25 17:52:10 +0530

    nft minting contract

--- token_program/programs/token_program/src/lib.rs
+++ token_program/programs/token_program/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token::{Mint, Token, TokenAccount}};
 
 declare_id!("6hRoxZFj1SPuiSnVNbuBoDeyDuTnVqUdDsU9QKiNMKoj");
 
@@ -6,36 +7,68 @@ declare_id!("6hRoxZFj1SPuiSnVNbuBoDeyDuTnVqUdDsU9QKiNMKoj");
 pub mod token_program {
     use super::*;
 
-    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+    pub fn createMint(ctx: Context<CreateMint>, amount: u64) -> Result<()> {
         // ...
         Ok(())
     }
 
-    pub fn withdraw(ctx: Context<VaultAction>) -> Result<()> {
+      pub fn createToken(ctx: Context<CreateToken>, amount: u64) -> Result<()> {
+        // ...
+        Ok(())
+    }
+
+      pub fn createATA(ctx: Context<CreateATA>, amount: u64) -> Result<()> {
         // ...
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct VaultAction<'info> {
+pub struct CreateMint<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        mint::decimals = 6,
+        mint::authority = signer.key(),
+    )]
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct CreateToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        token::mint = mint,
+        token::authority = signer,
+    )]
+    pub token: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct CreateATA<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
 
+    pub mint: Account<'info, Mint>,
+
     #[account(
-    mut,
-    seeds = [b"vault", signer.key().as_ref()],
-    bump,
-  )]
-    pub vault: SystemAccount<'info>,
+        init,
+        payer = signer,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub token: Account<'info, TokenAccount>,
 
     pub system_program: Program<'info, System>,
-}
-
-#[error_code]
-pub enum VaultError {
-    #[msg("Vault already exists")]
-    VaultAlreadyExists,
-    #[msg("Invalid amount")]
-    InvalidAmount,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
